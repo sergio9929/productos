e19725c24e9c28a572ace3fe3c9502e0d9cb96a7
Eider <Eider@eiderirakasle>
2020-03-29 23:12:13 +0200

    Cambio de base de datos

--- src/modelo/Config.java
+++ src/modelo/Config.java
@@ -1,7 +1,7 @@
 package modelo;
 
 public interface Config {
-	public static final String BBDD = "ud6_productos";
+	public static final String BBDD = "productos";
 	public static final String HOST = "localhost";
 	public static final String USERNAME = "root";
 	public static final String PASSWORD = "";
