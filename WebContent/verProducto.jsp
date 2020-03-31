d06736e26262adadf46dcad2b1e43d58d29021c5
sergi <sergi@DESKTOP-TVUII7J>
2020-03-31 12:22:24 +0200

    implementado verProductos y verProducto

---
+++ WebContent/verProducto.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+		<h1>Producto</h1>
+		<p><span class="font-weight-bold">Id:</span> ${producto.getId()}</p>
+		<p><span class="font-weight-bold">Nombre:</span> ${producto.getNombre()}</p>
+		<p><span class="font-weight-bold">Stock:</span> ${producto.getStock()}</p>
+		<p><span class="font-weight-bold">Fecha Compra:</span> ${producto.getFecha_compra()}</p>
+		<p><span class="font-weight-bold">Color:</span> ${producto.getColor()}</p>
+		<p><span class="font-weight-bold">Made In:</span> ${producto.getMade_in()}</p>
+		<p><span class="font-weight-bold">Precio:</span> ${producto.getPrecio()}</p>
+	</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
--- src/modelo/dao/ModeloProducto.java
+++ src/modelo/dao/ModeloProducto.java
@@ -1,20 +1,62 @@
 package modelo.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import modelo.bean.Producto;
 import modelo.bean.Talla;
+import modelo.Conector;
 
-public class ModeloProducto {
-
+public class ModeloProducto extends Conector {
 	public ArrayList<Producto> getAll(){
 		//TODO implementar
-		return null;
+		ArrayList<Producto> productos = new ArrayList<Producto>();
+		try {
+			PreparedStatement pstSelect = conexion.prepareStatement("select * from productos");
+			ResultSet rs=pstSelect.executeQuery();
+			while(rs.next()) {
+				Producto producto=new Producto();
+				producto.setId(rs.getInt("id"));
+				producto.setNombre(rs.getString("nombre"));
+				producto.setStock(rs.getInt("stock"));
+				producto.setFecha_compra(rs.getDate("fecha_compra"));
+				producto.setColor(rs.getString("color"));
+				producto.setMade_in(rs.getString("made_in"));
+				producto.setPrecio(rs.getDouble("precio"));
+				producto.setDescuento(rs.getInt("descuento"));
+				productos.add(producto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return productos;
 	}
 	
 	public Producto get(int id) {
 		//TODO implementar
-		return null;
+		Producto producto=new Producto();
+		try {
+			PreparedStatement pstSelect = conexion.prepareStatement("select * from productos where id=?");
+			pstSelect.setInt(1, id);
+			ResultSet rs=pstSelect.executeQuery();
+			while(rs.next()) {
+				producto.setId(rs.getInt("id"));
+				producto.setNombre(rs.getString("nombre"));
+				producto.setStock(rs.getInt("stock"));
+				producto.setFecha_compra(rs.getDate("fecha_compra"));
+				producto.setColor(rs.getString("color"));
+				producto.setMade_in(rs.getString("made_in"));
+				producto.setPrecio(rs.getDouble("precio"));
+				producto.setDescuento(rs.getInt("descuento"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return producto;
 	}
 	
 	public void insert(Producto producto) {
